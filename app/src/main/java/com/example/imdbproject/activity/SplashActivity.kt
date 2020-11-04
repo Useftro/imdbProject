97dfa3efbc2efe42e27aa4eaef670ec0d7c4bd2d
Unioltered <unioltered@gmail.com>
2020-11-04 15:38:19 +0300

    Initial commit. Splash screen ready.

---
+++ app/src/main/java/com/example/imdbproject/activity/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.imdbproject.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.imdbproject.MainActivity
+import com.example.imdbproject.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        },4000)
+
+
+    }
+}
