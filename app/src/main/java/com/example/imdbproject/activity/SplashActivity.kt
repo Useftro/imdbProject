1da460fd925859de1d2fe99ec603dc96e1c40819
Unioltered <unioltered@gmail.com>
2020-11-04 15:49:56 +0300

    fixed gitignore

--- app/src/main/java/com/example/imdbproject/activity/SplashActivity.kt
+++ app/src/main/java/com/example/imdbproject/activity/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.core.os.HandlerCompat.postDelayed
 import com.example.imdbproject.MainActivity
 import com.example.imdbproject.R
 
