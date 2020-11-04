884019c3d2990d4cc2d6887edfdc27d71a44832b
Unioltered <unioltered@gmail.com>
2020-11-04 19:08:17 +0300

    Added bottomNavigation.

--- app/src/main/java/com/example/imdbproject/MainActivity.kt
+++ app/src/main/java/com/example/imdbproject/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.imdbproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.ListFragment
+import com.example.imdbproject.fragment.FavoriteFragment
+import com.example.imdbproject.fragment.ProfileFragment
+import com.example.imdbproject.fragment.SettinsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = ListFragment()
+        val favoriteFragment = FavoriteFragment()
+        val profileFragment = ProfileFragment()
+        val settingsFragment = SettinsFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.ic_list -> makeCurrentFragment(homeFragment)
+                R.id.ic_favorite -> makeCurrentFragment(favoriteFragment)
+                R.id.ic_profile -> makeCurrentFragment(profileFragment)
+                R.id.ic_settings -> makeCurrentFragment(settingsFragment)
+            }
+            true
+        }
+
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+
 }
